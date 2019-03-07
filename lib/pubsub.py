f8905af8ad8fb8c7ff2820b2218798c1ed1cc86d
Matt Larner <mrlarner@gmail.com>
2019-03-06 22:32:56 -0800

    pubsub module

---
+++ lib/pubsub.py
@@ -0,0 +1,23 @@
+SUBSCRIBERS = dict()
+
+def _register_subscriber(event, callback):
+    if event in SUBSCRIBERS:
+        SUBSCRIBERS[event].push(callback)
+    else:
+        SUBSCRIBERS[event] = [callback]
+
+def sub(event):
+    def subscription(handler):
+        def decorated_handler(*args, **kwargs):
+            return handler(*args, **kwargs)
+        _register_subscriber(event, handler)
+        return decorated_handler
+    return subscription
+
+def pub(event, **kwargs):
+    if event in SUBSCRIBERS:
+        _subscribers = SUBSCRIBERS[event]
+        for subscriber in _subscribers:
+            subscriber(**kwargs)
+    else:
+        print('no subscribers for %s' % event)
